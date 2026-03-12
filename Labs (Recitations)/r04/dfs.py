04abbc6133a8db1b5b9dc44ffecf935c416b2c50
Victor Bankston <doogrammargood@gmail.com>
2026-03-12 15:00:57 -0500

    Updated lab4 readme.

--- Labs (Recitations)/r04/dfs.py
+++ Labs (Recitations)/r04/dfs.py
@@ -15,7 +15,7 @@ The Graph for sudoku. This is most naturally a Digraph.
 Vertices: partial assignments of Sudoku that are filled left to right.
 Edges: There is an edge from one partial assignment (A) to another (B) 
 if B comes from filling in the first empty square of A with a number.
-Hence, each vertex has 9 children, except for the leaves.
+Hence, each vertex has 9 children, except for the leaves, which are the total assignments.
 
 
 Code source: https://www.geeksforgeeks.org/dsa/depth-first-search-or-dfs-for-a-graph/
@@ -30,7 +30,7 @@ def dfsRec(adj, visited, s, res): #This corresponds to the backtrack function.
     for i in adj[s]:
         if not visited[i]:
             dfsRec(adj, visited, i, res)
-            
+
 def dfs(adj):
     visited = [False] * len(adj)
     res = []
